23d581822e369b5975beab554c5d3194edf2ab24
lucianunezalonso <nunezazlonsolucia@gmail.com>
2023-05-05 01:45:02 +0200

    Formulario modelo de recomendaciones: Actualizacion de variables, no funcional

--- app_tfg/lib/screens/opcion1_test.dart
+++ app_tfg/lib/screens/opcion1_test.dart
@@ -9,21 +9,29 @@ class _Opcion1TestState extends State<Opcion1Test> {
   // Inicialización de variables:
   final _formKey = GlobalKey<FormState>();
   String _especie = 'perro';
+  String _raza= '';
   String _sexo = 'macho';
   String _tamano = 'grande';
-  String _nivelActividad = 'alto';
-  String _ingreso = 'reciente';
+  String _tiempo = 'reciente';
   String _edad = 'cachorro';
-  bool? _vacunado = false;
-  bool? _desparasitado = false;
-  bool? _sano = false;
-  bool? _esterilizado = false;
   bool? _microchip = false;
 
+  int _years = 0;
+  int _months = 0;
+
+  final _yearsController = TextEditingController();
+  final _monthsController = TextEditingController();
 
   @override
+  void dispose() {
+    _yearsController.dispose();
+    _monthsController.dispose();
+    super.dispose();
+  }
+
+// Raza	Especie	Sexo	Tamaño	Microchip	Tiempo	Edad
+  @override
   Widget build(BuildContext context) {
-    final _scrollController = ScrollController();
 
     return Scaffold(
         appBar: AppBar(
@@ -32,188 +40,191 @@ class _Opcion1TestState extends State<Opcion1Test> {
         centerTitle: true,
       ),
 
-      body: Scrollbar(
-        controller: _scrollController,
-        isAlwaysShown: true,
-        thickness: 8.0,
-        radius: Radius.circular(10.0),
+      body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Especie',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0),),
+                DropdownButtonFormField(
+                  value: _especie,
+                  onChanged: (value) {
+                    setState(() {
+                        _especie = value.toString();
+                    });
+                  },
+                  items: <String>['perro', 'gato']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 16.0),
+                Text('Tamaño',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0),),
+                DropdownButtonFormField(
+                  value: _tamano,
+                  onChanged: (value) {
+                  setState(() {
+                    _tamano = value.toString();
+                  });
+                },
+                items: <String>['grande', 'mediano', 'pequeño']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 16.0),
 
-        child: SingleChildScrollView(
-            controller: _scrollController,
-
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Especie'),
-                    DropdownButtonFormField(
-                      value: _especie,
-                      onChanged: (value) {
-                        setState(() {
-                            _especie = value.toString();
-                        });
-                      },
-                      items: <String>['perro', 'cobaya', 'conejo', 'gato', 'hurón']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text('Sexo'),
-                    RadioListTile(
-                      title: Text('Macho'),
-                      value: 'macho',
-                      groupValue: _sexo,
-                      onChanged: (value) {
-                        setState(() {
-                          _sexo = value.toString();
-                        });
-                      },
-                    ),
-                    RadioListTile(
-                      title: Text('Hembra'),
-                      value: 'hembra',
-                      groupValue: _sexo,
-                      onChanged: (value) {
-                        setState(() {
-                          _sexo = value.toString();
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16.0),
-                    Text('Tamaño'),
-                    DropdownButtonFormField(
-                      value: _tamano,
-                      onChanged: (value) {
-                      setState(() {
-                        _tamano = value.toString();
-                      });
-                    },
-                    items: <String>['grande', 'mediano', 'pequeño']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16.0),
-                  Text('Nivel de actividad'),
-                  DropdownButtonFormField(
-                    value: _nivelActividad,
-                    onChanged: (value) {
-                      setState(() {
-                        _nivelActividad = value.toString();
-                      });
-                    },
-                    items: <String>['alto', 'medio', 'bajo']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16.0),
-                  Text('Ingreso'),
-                  DropdownButtonFormField(
-                    value: _ingreso,
-                    onChanged: (value) {
-                      setState(() {
-                        _ingreso = value.toString();
-                      });
-                    },
-                    items: <String>['reciente', 'medio', 'antiguo']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                    SizedBox(height: 16.0),
-                    Text('Edad'),
-                    DropdownButtonFormField(
-                      value: _edad,
-                      onChanged: (value) {
-                        setState(() {
-                          _edad = value.toString();
-                        });
-                      },
-                      items: <String>['cachorro', 'joven', 'adulto', 'senior']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 16.0),
-                    CheckboxListTile(
-                      title: Text('Vacunado'),
-                      value: _vacunado,
-                      onChanged: (value) {
-                        setState(() {
-                          _vacunado = value;
-                        });
-                      },
-                    ),
-                    CheckboxListTile(
-                      title: Text('Desparasitado'),
-                      value: _desparasitado,
-                      onChanged: (value) {
-                        setState(() {
-                          _desparasitado = value;
-                        });
-                      },
-                    ),
-                    CheckboxListTile(
-                      title: Text('Sano'),
-                      value: _sano,
-                      onChanged: (value) {
-                        setState(() {
-                          _sano = value;
-                        });
-                      },
-                    ),
-                    CheckboxListTile(
-                      title: Text('Esterilizado'),
-                      value: _esterilizado,
-                      onChanged: (value) {
-                        setState(() {
-                          _esterilizado = value;
-                        });
-                      },
-                    ),
-                    CheckboxListTile(
-                      title: Text('Microchip'),
-                      value: _microchip,
-                      onChanged: (value) {
-                        setState(() {
-                          _microchip = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16.0),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState?.validate() == true) {
-                          // Aquí tendrían que mandarse los datos al modelo
-                          }
+                Text('Sexo',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0),),
+                Row(
+                  children: [
+                    Flexible(
+                      child: RadioListTile(
+                        title: Text('Macho'),
+                        value: 'macho',
+                        groupValue: _sexo,
+                        onChanged: (value) {
+                          setState(() {
+                            _sexo = value.toString();
+                          });
                         },
-                        child: Text('Enviar'),
+                      ),
+                    ),
+                    Flexible(
+                      child: RadioListTile(
+                        title: Text('Hembra'),
+                        value: 'hembra',
+                        groupValue: _sexo,
+                        onChanged: (value) {
+                          setState(() {
+                            _sexo = value.toString();
+                          });
+                        },
                       ),
                     ),
                   ],
                 ),
-              ),
+
+                Text('Edad',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0),),
+                DropdownButtonFormField(
+                  value: _edad,
+                  onChanged: (value) {
+                    setState(() {
+                      _edad = value.toString();
+                    });
+                  },
+                  items: <String>['cachorro', 'joven', 'adulto', 'senior']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 16.0),
+
+
+                const Text(
+                  'Tiempo en estado de adopción',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  children: [
+                    Flexible(
+                      child: TextFormField(
+                        controller: _yearsController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: 'Años',
+                          hintText: '0-12',
+                        ),
+                        validator: (value) {
+                          final years = int.tryParse(value ?? '');
+                          if (years == null || years < 0 || years > 12) {
+                            return 'Ingresa un valor entre 0 y 12';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _years = int.tryParse(value ?? '') ?? 0;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Flexible(
+                      child: TextFormField(
+                        controller: _monthsController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: 'Meses',
+                          hintText: '0-11',
+                        ),
+                        validator: (value) {
+                          final months = int.tryParse(value ?? '');
+                          if (months == null || months < 0 || months > 11) {
+                            return 'Ingresa un valor entre 0 y 11';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _months = int.tryParse(value ?? '') ?? 0;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+
+                CheckboxListTile(
+                  title: const Text('Microchip',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0),),
+                  value: _microchip,
+                  onChanged: (value) {
+                    setState(() {
+                      _microchip = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 1.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() == true) {
+                      // Aquí tendrían que mandarse los datos al modelo
+                      }
+                    },
+                    child: Text('Enviar'),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
